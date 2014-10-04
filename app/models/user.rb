20eb7fc804b2a6d4d9a2d03490f6e3978cf2bf8b
Ward Minnis <ward@minnis.net>
2014-10-04 17:49:23 -0400

    Got the am_writing? function working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ActiveRecord::Base
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 	has_many :projects
+	has_many :entries, through: :projects
+
+	def type_is
+		if genre_id
+			type_of_writer = Genre.find_by id: genre_id
+			type_of_writer.name
+		end
+	end
+
+	def am_writing?
+			times_written = entries.where(created_at: (Time.now.midnight - desired_interval.to_i.day)..Time.now.midnight).length
+			
+			if times_written >= desired_amount
+				writing = true
+			else
+				writing = false
+			end
+		writing
+	end
 end
